e5b18b4fbf390066ee97c1a75d0fb1c7325f4a3d
daramzy <daramzycoder@gmail.com>
2021-10-03 19:08:13 +0900

    baekjoon

--- 16236번 아기상어 G4.py
+++ 16236번 아기상어 G4.py
@@ -1,13 +1,54 @@
 from sys import stdin
 from collections import deque
-import heapq
 
 n = int(stdin.readline())
 sea = [list(map(int, stdin.readline().split())) for i in range(n)]
 
+shark = 2
+count = 0
+fish = []
+eat = 0
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+def bfs(x,y):
+    global fish
+    fish = []
+    check = [[True for i in range(n)] for j in range(n)]
+    q = deque()
+
+    q.append([0, x, y])
+    check[x][y] = False
+    while q:
+        d, x, y = q.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0<=nx<n and 0<=ny<n and check[nx][ny] and sea[nx][ny]<=shark:
+                q.append([d+1, nx, ny])
+                check[nx][ny] = False
+                if 0< sea[nx][ny] < shark:
+                    fish.append([d+1, nx, ny])
 
 
+x, y = 0, 0
+for i in range(n):
+    for j in range(n):
+        if sea[i][j] == 9:
+            x,y = i,j
 
+while True:
+    bfs(x,y)
+    if len(fish) == 0:
+        break
+    fish.sort()
+    count+=fish[0][0]
+    eat+=1
+    if eat == shark:
+        shark+=1
+        eat = 0
+    sea[x][y] = 0
+    x, y = fish[0][1], fish[0][2]
+print(count)
 '''
 꼭 크기 순으로 먹어야되는건 아니다
 먹을 수 있는 모든 물고기 중 가장 가까운 것

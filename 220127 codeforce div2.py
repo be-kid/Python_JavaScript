73fe2c7e5292d09fcb0fe63f69c44186e6f769e7
LeeJooYong <daramzycoder@gmail.com>
2022-01-28 00:19:52 +0900

    220127 codeforce div2 1문제 품... 벽느껴진다...

---
+++ 220127 codeforce div2.py
@@ -0,0 +1,15 @@
+from sys import stdin 
+
+test = int(stdin.readline())
+
+# Problem A
+# for t in range(test):
+#     n = int(stdin.readline())
+#     a = list(map(int, stdin.readline().split()))
+#     b = list(map(int, stdin.readline().split()))
+    
+#     for i in range(n):
+#         if a[i] < b[i]:
+#             a[i], b[i] = b[i], a[i];
+    
+#     print(max(a)*max(b))
